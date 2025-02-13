21d6f377ca43c4eeb6af50f2b3fe07412459565d
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-02-13 10:35:48 +0100

    add promt number

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 /* L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore */
 
+const numeroUno = Number(prompt("Inserisci il primo numero"))
+console.log(numeroUno)
+const numeroDue = Number(prompt("Inserisci il secondo numero"))
+console.log(numeroDue)
+
+
